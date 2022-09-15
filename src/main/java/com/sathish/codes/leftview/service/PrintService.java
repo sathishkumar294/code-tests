776a8c6dc8a3756a7597ed226214c6c504bb1775
Sathish Kumar <sathishkumar.aero51@gmail.com>
2022-09-16 00:35:50 +0530

    Fix print binary tree

--- src/main/java/com/sathish/codes/leftview/service/PrintService.java
+++ src/main/java/com/sathish/codes/leftview/service/PrintService.java
@@ -4,124 +4,115 @@ import com.sathish.codes.leftview.model.Node;
 import com.sathish.codes.leftview.model.Tree;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.util.*;
 
 public class PrintService {
 
     public PrintService() {
-        points = new ArrayList<>();
+        plotTree = new PlotTree();
     }
 
-    private List<Point> points;
+    private final PlotTree plotTree;
 
     public void printTree(Tree tree) {
         Node startNode = tree.getStartNode();
-        processPointsForNode(startNode, false);
+        processLinesForNode(startNode, 1);
     }
 
-    public void processPointsForNode(Node node, boolean isRightNode) {
+    public void processLinesForNode(Node node, int lineNo) {
         if (node != null) {
-            Point nodePoint = getPointForNode(node);
-            Point parentPoint = getPointForNode(node.getParentNode() != null ? node.getParentNode() : null);
-            if (nodePoint.getX() == 0 && nodePoint.getY() == 0) {
-                int x = parentPoint != null ? parentPoint.getX() : 1;
-                int y = parentPoint != null ? parentPoint.getY() : 0;
-                nodePoint = new Point(x + (parentPoint != null ? 1 : 0) + (isRightNode ? 1 : 0), y + 1, node);
-                // Move the parent if exists to right
-                if (parentPoint != null && !isRightNode) {
-                    updateParentSpacing(node, 1 + (parentPoint != null ? 1 : 0));
-                }
-                points.add(nodePoint);
-            }
-            processPointsForNode(node.getLeftNode(), false);
-            processPointsForNode(node.getRightNode(), true);
-
-
-            printPoints();
+            plotTree.addNode(node, lineNo);
+            System.out.println("\n\n=================================================");
+            printPlotTree();
+            processLinesForNode(node.getLeftNode(), lineNo + 1);
+            processLinesForNode(node.getRightNode(), lineNo + 1);
         }
-        System.out.println("\n\n=================================================");
 
     }
 
-    private void printPoints() {
+    private void printPlotTree() {
+        List<Point> points = plotTree.getPoints();
         Map<Integer, List<Point>> pointsGroups = new HashMap<>();
         points.forEach(point -> {
-            if (pointsGroups.get(point.getY()) == null) {
-                pointsGroups.put(point.getY(), new ArrayList<>());
-            }
+            pointsGroups.computeIfAbsent(point.getY(), k -> new ArrayList<>());
             pointsGroups.get(point.getY()).add(point);
         });
-        pointsGroups.forEach((groupNo, points) -> {
-            points.sort(Comparator.comparingInt(Point::getX));
-        });
-//        pointsGroups.forEach((groupNo, points) -> {
-//            for (int i = 1; i < points.size(); i++) {
-//                Point thisPoint = points.get(i);
-//                Point prevPoint = points.get(i - 1);
-//                thisPoint.setX(thisPoint.getX() - prevPoint.getX());
-//            }
-//        });
-        pointsGroups.values().stream().sorted(Comparator.comparingInt(pg -> pg.get(0).getY())).forEach(points -> {
-            System.out.println("\n");
-            for (int i = 0; i < points.size(); i++) {
-                Point point = points.get(i);
-                Point prevPoint = i > 1 ? points.get(i - 1) : null;
+        pointsGroups.forEach((groupNo, gPoints) -> gPoints.sort(Comparator.comparingInt(Point::getX)));
+        pointsGroups.values().stream().sorted(Comparator.comparingInt(pg -> pg.get(0).getY())).forEach(gPoints -> {
+            System.out.println("");
+            for (int i = 0; i < gPoints.size(); i++) {
+                Point point = gPoints.get(i);
+                Point prevPoint = i > 0 ? gPoints.get(i - 1) : null;
                 int spaceToLeave = prevPoint != null ? point.getX() - prevPoint.getX() : point.getX();
-                System.out.println(point.node.getValue() + " :(" + point.x + ", " +point.y+")");
-//                System.out.printf("%1$" + (spaceToLeave > 0 ? spaceToLeave : "") + "s", point.getNode().getValue());
-            };
+//                System.out.println(point.node.getValue() + " :(" + point.x + ", " + point.y + ")");
+                System.out.printf("%1$" + (spaceToLeave > 0 ? spaceToLeave : "") + "s", point.getNode().getValue());
+            }
         });
     }
 
-    public void updateParentSpacing(Node node, Integer spaceToAdd) {
-        if (node == null) return;
-        Point point = getPointForNode(node);
-        if (point != null) {
-            point.setX(point.getX() + spaceToAdd);
-            updateParentSpacing(node.getParentNode(), spaceToAdd);
-            // Move only the right node, as this method is triggered by left node movement
-            updateChildSpacing(node.getRightNode(), spaceToAdd);
-        }
-    }
-
-    public void updateChildSpacing(Node node, Integer spaceToMove) {
-        if (node == null) return;
-        Point point = getPointForNode(node);
-        if (point != null) {
-            point.setX(point.getX() + spaceToMove);
-            updateChildSpacing(node.getLeftNode(), spaceToMove);
-            updateChildSpacing(node.getRightNode(), spaceToMove);
-        }
-
-    }
-
-    private Point getPointForNode(Node node) {
-        if (node == null) return null;
-        return this.points.stream().filter(point -> point.getNode().getId().equals(node.getId())).findFirst().orElse(new Point(0, 0, node));
-    }
 
     @Data
-    class TreeDiagram {
+    static
+    class PlotTree {
         List<Point> points = new ArrayList<>();
+
+        public void addNode(Node node, int lineNo) {
+            var point = points.stream().filter(p -> p.getNode().getId().equals(node.getId())).findFirst();
+            if (point.isEmpty()) {
+                int maxX = points.stream().filter(p -> p.getY() == lineNo).map(Point::getX).reduce(Math::max).orElse(0);
+                Point parentPoint = getPointForNode(node.getParentNode());
+                Point leftPoint = getPointForNode(node.getLeftNode());
+                Point rightPoint = getPointForNode(node.getRightNode());
+                Point pt = new Point((maxX > 0 ? maxX + 2 : 1), lineNo, node, parentPoint, leftPoint, rightPoint);
+                points.add(pt);
+                if (parentPoint != null) {
+                    adjustParentPosition(parentPoint);
+                }
+            }
+        }
+
+        void adjustParentPosition(Point parentPoint) {
+            if (parentPoint != null) {
+                Point leftPoint = getPointForNode(parentPoint.getNode().getLeftNode());
+                Point rightPoint = getPointForNode(parentPoint.getNode().getRightNode());
+                int newX = 0;
+                if (leftPoint.getX() != 0 && rightPoint.getX() == 0) {
+                    newX = rightPoint.getX();
+                } else if (leftPoint.getX() == 0 && rightPoint.getX() != 0) {
+                    newX = rightPoint.getX();
+                } else {
+                    newX = (leftPoint.getX() + rightPoint.getX()) / 2;
+                }
+                parentPoint.setX(newX);
+                adjustParentPosition(parentPoint.getParentPoint());
+            }
+        }
+
+        private Point getPointForNode(Node node) {
+            if (node == null) return null;
+            return this.points.stream().filter(point -> point.getNode().getId().equals(node.getId())).findFirst().orElse(new Point(0, 0, node));
+        }
     }
 
     @Data
     @AllArgsConstructor
+    @RequiredArgsConstructor
+    static
     class Point {
+        @NonNull
         private int x;
+        @NonNull
         private int y;
+        @NonNull
         private Node node;
-    }
 
-    @Data
-    @AllArgsConstructor
-    class Segment {
-        private String nodeId;
-        private String value;
-        private Integer noOfSpace;
-        private Integer lineNo;
-    }
+        private Point parentPoint;
 
+        private Point leftChildPoint;
+        private Point rightChildPoint;
+    }
 
 }
